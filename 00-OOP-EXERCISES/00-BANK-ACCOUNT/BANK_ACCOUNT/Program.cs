4ce93ed9c4b81b3ffd7dc725e93138d9c11fed7d
kyweez <auboudier.pro@gmail.com>
2020-07-10 23:17:53 +0200

    #1-Bank-account / Init Doc

---
+++ 00-OOP-EXERCISES/00-BANK-ACCOUNT/BANK_ACCOUNT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BANK_ACCOUNT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
