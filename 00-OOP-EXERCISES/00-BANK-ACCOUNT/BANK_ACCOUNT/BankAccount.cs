09f6797d86a3f45cb2587f84277785dff769302c
kyweez <auboudier.pro@gmail.com>
2020-07-10 23:57:43 +0200

    #1-Bank-account / Push before sleeping

---
+++ 00-OOP-EXERCISES/00-BANK-ACCOUNT/BANK_ACCOUNT/BankAccount.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BANK_ACCOUNT
+{
+    class BankAccount
+    {
+        //===================== ATTRIBUTES =====================//
+        private string accountNumber;
+        private string owner;
+        private float balance;
+        private float authorizedOverdraft;
+
+        //===================== CONSTRUCTORS ====================//
+        /// <summary>
+        /// BankAccount constructor
+        /// </summary>
+        /// <param name="_accountNumber">accountNumber value</param>
+        /// <param name="_owner">owner value</param>
+        public BankAccount(string _accountNumber, string _owner)
+        {
+            accountNumber = _accountNumber;
+            owner = _owner;
+            balance = 0f;
+            authorizedOverdraft = 0f;
+        }
+
+        //================== GETTERS & SETTERS =================//
+        /// <summary>
+        /// acccountNumber getter
+        /// </summary>
+        public string AccountNumber { get; }
+
+        /// <summary>
+        /// owner getter
+        /// </summary>
+        public string Owner { get; }
+
+        /// <summary>
+        /// balance getter and setter
+        /// </summary>
+        public string Balance { get; set; }
+
+        /// <summary>
+        /// authorizedOverdraft getter and setter
+        /// </summary>
+        public string AuthorizedOverdraft { get; set; }
+
+        //======================= METHODS ======================//
+        public void PrintInformation()
+        {
+
+        }
+
+        public void Credit(float _string)
+        {
+
+        }
+
+        public bool Debit(float _amount)
+        {
+            return true;
+        }
+
+        public bool Transfer(BankAccount _account, float _amount)
+        {
+            return true;
+        }
+
+        public bool balanceIsHigher(BankAccount _account)
+        {
+            return true;
+        }
+    }
+}
