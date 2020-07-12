db2bf2d6830bcaf294f05f7a53465714fa26aacf
kyweez <auboudier.pro@gmail.com>
2020-07-12 23:47:39 +0200

    #1-Bank-account / Init Test

--- 00-OOP-EXERCISES/00-BANK-ACCOUNT/BANK_ACCOUNT/Program.cs
+++ 00-OOP-EXERCISES/00-BANK-ACCOUNT/BANK_ACCOUNT/Program.cs
@@ -6,7 +6,18 @@ namespace BANK_ACCOUNT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\x1b[36mCreating 2 bank accounts :");
+            Console.WriteLine("==========================\x1b[0m");
+            Console.WriteLine("\x1b[35mBankAccount account_aurel = new BankAccount(\"001\", \"Aurel\");");
+            Console.WriteLine("BankAccount account_mike = new BankAccount(\"002\", \"Mike\");\x1b[0m");
+            BankAccount account_aurel = new BankAccount("001", "Aurel");
+            BankAccount account_mike = new BankAccount("002", "Mike");
+            Console.WriteLine("\n---------------------------------------------------------------------\n");
+
+            Console.WriteLine("\x1b[36mSetters testing :");
+            Console.WriteLine("=================\x1b[0m");
+            account_aurel.Balance = 20000.123f;
+            account_aurel.AuthorizedOverdraft = 700; 
         }
     }
 }
