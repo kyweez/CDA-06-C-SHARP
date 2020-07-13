55baacfba663a9780ea212ca56609117e562802f
kyweez <auboudier.pro@gmail.com>
2020-07-13 23:59:59 +0200

    #3-Fraction / Init Code

---
+++ 00-OOP-EXERCISES/02-FRACTION/FRACTION/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FRACTION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
