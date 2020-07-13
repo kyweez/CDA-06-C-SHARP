76bf4508c5d487d1e7d54d4c7f5a1c57ba96b54a
kyweez <auboudier.pro@gmail.com>
2020-07-13 23:07:26 +0200

    #2-Point / Code OK - neeed tests

--- 00-OOP-EXERCISES/01-POINT/POINT/Point.cs
+++ 00-OOP-EXERCISES/01-POINT/POINT/Point.cs
@@ -2,6 +2,64 @@
 {
     class Point
     {
+        //===================== ATTRIBUTES =====================//
+        private int x;
+        private int y;
 
+        //===================== CONSTRUCTORS ====================//
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        //======================= METHODS ======================//
+        public override string ToString() => ("(" + x + ";" + y + ")");
+
+        public void PrintCoordinate() => System.Console.WriteLine("Point coordinates : " + this.ToString());
+
+        public void Move(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public Point OrdinateAxialSymmetry()
+        {
+            if (x == int.MinValue)
+                return new Point(x, y);
+            return new Point(-x, y);
+        }
+
+        public Point AbcissaAxialSymmetry()
+        {
+            if (y == int.MinValue)
+                return new Point(x, y);
+            return new Point(x, -y);
+        }
+
+        public Point CentralSymmetry()
+        {
+            if (x == int.MinValue || y == int.MinValue)
+                return new Point(x, y);
+            return new Point(-x, -y);
+        }
+
+        public void swapCoordinate()
+        {
+            int tmp;
+            if (x == int.MinValue || y == int.MinValue)
+                return;
+            tmp = x;
+            x = y;
+            y = tmp;
+
+        }
     }
 }
